3eae39b698dfb895837c9b7b0354e9f4cf4c9bea
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 16:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
+
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  // create the list
+  const list = []
+  for (let i = 0; i < line.length; i++) {
+    list.push(`${i+1}. ${line[i]}`)
+  }
+
+  return `The line is currently ${list.join(', ')}`
+
   if (line.length === 0 ) {
     return "The line is currently empty."
   }
