6c7ea0a8711f0bf9aa2d921bf3dc3a177eb0652b
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 16:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(line) {
     list.push(`${i+1}. ${line[i]}`)
   }
 
-  return `The line is currently ${list.join(', ')}`
+  return `The line is currently: ${list.join(', ')}`
 
   if (line.length === 0 ) {
     return "The line is currently empty."
